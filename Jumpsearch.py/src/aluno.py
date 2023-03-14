9af0fe024819e6cfe99736636ac74d3ac488a958
JhoanDev <jhoandev.log@gmail.com>
2023-03-14 10:13:31 -0300

    feat: sla

--- Jumpsearch.py/src/aluno.py
+++
@@ -1,6 +0,0 @@
-class Aluno:
-    def __init__(self, nome, matricula, notas):
-        self.nome = nome
-        self.matricula = matricula
-        self.notas = notas
-        self.media = sum(self.notas) / len(self.notas)
