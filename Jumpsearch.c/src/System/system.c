ebf7cc1e68b381790c911db1f365641abbfbc190
JhoanDev <jhoandev.log@gmail.com>
2024-02-22 08:55:07 -0300

    refact: pequeno erro no menu

--- Jumpsearch.c/src/System/system.c
+++ Jumpsearch.c/src/System/system.c
@@ -198,9 +198,7 @@ void menu(void)
     printf("[3] Buscar aluno por matricula\n");
     printf("[4] Buscar aluno pelo nome\n");
     printf("[5] Sair\n");
-    printf("========================================\n");
-    printf("Digite o numero da opcao desejada: ");
-}
+    printf("========================================\n");}
 
 void print_green(char *text)
 {
